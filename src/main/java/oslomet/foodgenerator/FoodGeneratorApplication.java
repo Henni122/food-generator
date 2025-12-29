f6c8457d09bc75b4a4a17eeb8ba6ef71e07607c4
Henni <henniabdulhafiz@gmail.com>
2025-12-29 19:06:52 +0100

    Initial commit

---
+++ src/main/java/oslomet/foodgenerator/FoodGeneratorApplication.java
@@ -0,0 +1,13 @@
+package oslomet.foodgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oslomet/foodgenerator/FoodGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.foodgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
